40d3c147bba223d1067c3571126728096cc52522
forteploetzlich01 <adrianpoelzl@gmail.com>
2025-11-27 10:37:05 +0100

    Add click event to surprise button

--- java.js
+++ java.js
@@ -1,1 +1,6 @@
+document.getElementById('surpriseButton').addEventListener('click', function() {
+    const greetingElement = document.getElementById('greeting');
+    greetingElement.textContent = 'Surprise! You clicked the button!';
+    greetingElement.style.color = 'blue';
+});
 
